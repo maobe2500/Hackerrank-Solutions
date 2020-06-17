0cb86dc965fa4984c4507c5f6358851a56841cc0
maobe2500 <53196182+maobe2500@users.noreply.github.com>
2020-06-17 19:21:15 +0200

    Update Plus-minus.js

--- Plus-minus.js
+++ Plus-minus.js
@@ -1,5 +1,9 @@
 function plusMinus(arr) {
-    let list = [0,0,0]
+    
+    //Initialize a list with zeros for the posetive, negative and zeros
+    let list = [0,0,0];
+    
+    //Go through the "arr" array, check them and add 1 to their place in "list" 
     arr.forEach(num => {
         if (num > 0) {
             list[0] += 1;
@@ -8,11 +12,12 @@ function plusMinus(arr) {
         } else {
             list[2] += 1;
         }
-    })
+    });
     
+    //Calculate the percentage of each and log to the console
     let pos = list[0] / arr.length,
         neg = list[1] / arr.length, 
         zero = list[2] / arr.length;
-
+    
     console.log(`${pos}\n${neg}\n${zero}`);
 }
